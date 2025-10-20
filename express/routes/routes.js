4e2367d05d190a38e704335316c1aa4ec067f689
nbinh123 <133865193+nbinh123@users.noreply.github.com>
2025-10-20 18:42:18 +0700

    ok

---
+++ express/routes/routes.js
@@ -0,0 +1,10 @@
+// import các controller 
+const productRoute = require("../routes/route/productRoute")
+const userRoute = require("../routes/route/userRoute")
+
+function router(app){ 
+    app.use("/products", productRoute)
+    app.use("/user",  userRoute)
+}
+
+module.exports = router
